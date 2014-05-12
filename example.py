6619b87fffe063fcbaf29daabb51dafb03e34f2f
Nicolas Limage <github@xephon.org>
2014-05-12 11:35:04 +0200

    cleaned and added more comments

--- example.py
+++ example.py
@@ -1,23 +1,47 @@
 #!/usr/bin/env python
+""" Shinkenplugin example plugin """
 
 import shinkenplugin
 
-if __name__ == "__main__":
+def main():
+
+    # Create a new shinkenplugin.Plugin instance
     sp = shinkenplugin.Plugin()
-    sp.add_arg('-w', '--warning', nargs=1, metavar="THRESHOLD", type=str)
-    sp.add_arg('-c', '--critical', nargs=1, metavar="THRESHOLD", type=str)
-    sp.add_arg('--value', nargs=1, required=1, metavar="VALUE", type=int)
+
+    # Add some arguments to parse
+    sp.add_arg('-w', '--warning', metavar="THRESHOLD", type=str,
+        help="Warning Threshold, see https://nagios-plugins.org/doc/guidelines.html#THRESHOLDFORMAT")
+    sp.add_arg('-c', '--critical', metavar="THRESHOLD", type=str,
+        help="Warning Threshold, see https://nagios-plugins.org/doc/guidelines.html#THRESHOLDFORMAT")
+    sp.add_arg('--value', required=1, metavar="VALUE", type=int)
+
+    # Parse the arguments
     sp.parse_args()
+
+    # Set plugin timeout
     sp.set_timeout()
 
-    value = sp.args.value[0]
-    t_warn = sp.args.warning[0]
-    t_crit = sp.args.critical[0]
+    # Get values and thresholds from command line
+    value = sp.args.value
+    t_warn = sp.args.warning
+    t_crit = sp.args.critical
 
+    # Check value against thresholds
     code = sp.check_threshold(value, warning=t_warn, critical=t_crit)
+
+    # Add result to the stack
     sp.add_result(code, "value=%d" % value)
+
+    # Add some performance data to the stack
     sp.add_perfdata("value", value, warning=t_warn, critical=t_crit, minimum=0, maximum=100)
+
+    # Add some extra data if needed
     if sp.args.verbose > 2:
         sp.add_extdata("value has been determined to be %d" % (value))
 
+    # Exit printing the conclusions in a nagios-plugin compliant way
     sp.finish()
+
+
+if __name__ == "__main__":
+    main()
