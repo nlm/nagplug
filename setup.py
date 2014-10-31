044d375b19dc7e2a134cf33657e5e5f73331c97a
Nicolas Limage <github@xephon.org>
2014-10-31 16:13:25 +0100

    added setuptools install script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "nagplug",
+    version = "1.0.0",
+    packages = find_packages(),
+    
+    author = "Nicolas Limage",
+    description = "Nagios guidelines-compliant plugin creation library (Shinken, Icinga, Centreon)",
+    license = "GPL",
+    keyworkds = "nagios plugin shinken icinga centreon",
+    url = "https://github.com/nlm/nagplug",
+)
