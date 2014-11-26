dbe2386446459d636b2004f79bbf5d611c6a78b7
Nicolas Limage <github@xephon.org>
2014-11-26 12:51:22 +0100

    switched to MIT license and added classifiers

--- setup.py
+++ setup.py
@@ -6,8 +6,17 @@ setup(
     packages = find_packages(),
     author = "Nicolas Limage",
     description = "Nagios guidelines-compliant plugin creation library (Shinken, Icinga, Centreon)",
-    license = "GPL",
+    license = "MIT",
     keywords = "nagios plugin shinken icinga centreon",
     url = "https://github.com/nlm/nagplug",
     test_suite = 'test_nagplug',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Monitoring',
+    ],
 )
