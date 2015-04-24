6adbc716cb7d2bb2ce4a853c195c19a9ba2308d1
Nicolas Limage <github@xephon.org>
2015-04-24 15:53:40 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nagplug",
-    version="1.0.2",
-    packages=find_packages(),
-    author="Nicolas Limage",
-    description="Nagios guidelines-compliant plugin creation library"
-                " (Shinken, Icinga, Centreon)",
-    license="MIT",
-    keywords="nagios plugin shinken icinga centreon",
-    url="https://github.com/nlm/nagplug",
-    test_suite='test_nagplug',
-    classifiers=[
+    name = "nagplug",
+    version = "1.0.3",
+    packages = find_packages(),
+    author = "Nicolas Limage",
+    description = "Nagios guidelines-compliant plugin creation library (Shinken, Icinga, Centreon)",
+    license = "MIT",
+    keywords = "nagios plugin shinken icinga centreon",
+    url = "https://github.com/nlm/nagplug",
+    test_suite = 'test_nagplug',
+    classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
